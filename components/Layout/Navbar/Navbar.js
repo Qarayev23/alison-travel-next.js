16e61c32e90f0fac306eab99c597dcae99ce5f88
Nijat Garayev <qarayevn61@gmail.com>
2024-09-13 15:59:35 +0400

    update

--- components/Layout/Navbar/Navbar.js
+++ components/Layout/Navbar/Navbar.js
@@ -48,90 +48,98 @@ const Navbar = () => {
                     </Link>
                 </div>
                 <nav className={`${styles.nav} ${showNavbar ? styles.show : ''}`}>
-                    <Link href='/' className={styles.nav__logo}>
-                        <Image src="/images/logo.svg" alt="Alison Travel" fill priority />
-                    </Link>
-
-                    <div className={styles.nav__links}>
-                        <div className={styles.nav__links__desk}>
-                            <ul className={styles.nav__list}>
-                                <li className={styles.nav__item}>
-                                    <Link href='/' className={styles.nav__link}>Home</Link>
-                                </li>
-                                <li className={styles.nav__item}>
-                                    <Link href='/' className={styles.nav__link}>Our guests</Link>
-                                </li>
-                                <li className={styles.nav__item}>
-                                    <Link href='/' className={styles.nav__link}>Contact</Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className={styles.nav__links__mob}>
-                            <ul className={styles.nav__list}>
-                                <li className={styles.nav__list__title}>
-                                    Useful links
-                                </li>
-                                <li className={styles.nav__item}>
-                                    <Link href='/' className={styles.nav__link}>Home</Link>
-                                </li>
-                                <li className={styles.nav__item}>
-                                    <Link href='/' className={styles.nav__link}>Contacts</Link>
-                                </li>
-                                <li className={styles.nav__item}>
-                                    <Link href='/' className={styles.nav__link}>Cancellation policy</Link>
-                                </li>
-                            </ul>
-
-
-                            <ul className={styles.nav__list}>
-                                <li className={styles.nav__list__title}>
-                                    Information
-                                </li>
-                                <li className={styles.nav__item}>
-                                    <Link href='/' className={styles.nav__link}>About us</Link>
-                                </li>
-                                <li className={styles.nav__item}>
-                                    <Link href='/' className={styles.nav__link}>Our guests</Link>
-                                </li>
-                                <li className={styles.nav__item}>
-                                    <Link href='/' className={styles.nav__link}>Travel news</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div className={styles.nav__right}>
-                        <div className={styles.lang} ref={dropdownRef}>
-                            <button className={styles.lang__btn} onClick={handleShowLangDropdown}>
-                                <Image src="/images/lang.svg" alt="Language" width={15} height={15} priority />
-                                <span className={styles.lang__btn__text}>EN</span>
-                            </button>
-                            <ul className={`${styles.lang__dropdown} ${showLangDropDown ? styles.show : ''}`}>
-                                <li className={styles.lang__dropdown__item}>
-                                    <button className={styles.lang__dropdown__btn} onClick={handleShowLangDropdown}>
-                                        AZ
-                                    </button>
-                                </li>
-                                <li className={styles.lang__dropdown__item}>
-                                    <button className={styles.lang__dropdown__btn} onClick={handleShowLangDropdown}>
-                                        RU
-                                    </button>
-                                </li>
-                                <li className={styles.lang__dropdown__item}>
-                                    <button className={styles.lang__dropdown__btn} onClick={handleShowLangDropdown}>
-                                        UA
-                                    </button>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <Link href='/' className={styles.nav__fav}>
-                            <SvgHeart />
+                    <div className={styles.nav__wrapper}>
+                        <Link href='/' className={styles.nav__logo}>
+                            <Image src="/images/logo.svg" alt="Alison Travel" fill priority />
                         </Link>
 
-                        <Link href='/' className={styles.nav__b2b}>
-                            B2B
-                        </Link>
+                        <div className={styles.nav__links}>
+                            <div className={styles.nav__links__desk}>
+                                <ul className={styles.nav__list}>
+                                    <li className={styles.nav__item}>
+                                        <Link href='/' className={styles.nav__link}>Home</Link>
+                                    </li>
+                                    <li className={styles.nav__item}>
+                                        <Link href='/' className={styles.nav__link}>Our guests</Link>
+                                    </li>
+                                    <li className={styles.nav__item}>
+                                        <Link href='/' className={styles.nav__link}>Contact</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className={styles.nav__links__mob}>
+                                <ul className={styles.nav__list}>
+                                    <li className={styles.nav__list__title}>
+                                        Useful links
+                                    </li>
+                                    <li className={styles.nav__item}>
+                                        <Link href='/' className={styles.nav__link}>Home</Link>
+                                    </li>
+                                    <li className={styles.nav__item}>
+                                        <Link href='/' className={styles.nav__link}>Contacts</Link>
+                                    </li>
+                                    <li className={styles.nav__item}>
+                                        <Link href='/' className={styles.nav__link}>Cancellation policy</Link>
+                                    </li>
+                                </ul>
+
+
+                                <ul className={styles.nav__list}>
+                                    <li className={styles.nav__list__title}>
+                                        Information
+                                    </li>
+                                    <li className={styles.nav__item}>
+                                        <Link href='/' className={styles.nav__link}>About us</Link>
+                                    </li>
+                                    <li className={styles.nav__item}>
+                                        <Link href='/' className={styles.nav__link}>Our guests</Link>
+                                    </li>
+                                    <li className={styles.nav__item}>
+                                        <Link href='/' className={styles.nav__link}>Travel news</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className={styles.nav__right}>
+                            <div className={styles.lang} ref={dropdownRef}>
+                                <button className={styles.lang__btn} onClick={handleShowLangDropdown}>
+                                    <Image src="/images/lang.svg" alt="Language" width={15} height={15} priority />
+                                    <span className={styles.lang__btn__text}>EN</span>
+                                </button>
+                                <ul className={`${styles.lang__dropdown} ${showLangDropDown ? styles.show : ''}`}>
+                                    <li className={styles.lang__dropdown__item}>
+                                        <button className={styles.lang__dropdown__btn} onClick={handleShowLangDropdown}>
+                                            AZ
+                                        </button>
+                                    </li>
+                                    <li className={styles.lang__dropdown__item}>
+                                        <button className={styles.lang__dropdown__btn} onClick={handleShowLangDropdown}>
+                                            RU
+                                        </button>
+                                    </li>
+                                    <li className={styles.lang__dropdown__item}>
+                                        <button className={styles.lang__dropdown__btn} onClick={handleShowLangDropdown}>
+                                            UA
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <Link href='/' className={styles.nav__fav}>
+                                <SvgHeart />
+                            </Link>
+
+                            <Link href='/' className={styles.nav__b2b}>
+                                B2B
+                            </Link>
+                        </div>
+
+                        <button className={styles.nav__close} onClick={handleShowNavbar}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M0.292893 0.292893C0.683417 -0.0976309 1.31658 -0.0976309 1.70711 0.292893L7 5.58579L12.2929 0.292893C12.6834 -0.0976311 13.3166 -0.0976311 13.7071 0.292893C14.0976 0.683417 14.0976 1.31658 13.7071 1.70711L8.41421 7L13.7071 12.2929C14.0976 12.6834 14.0976 13.3166 13.7071 13.7071C13.3166 14.0976 12.6834 14.0976 12.2929 13.7071L7 8.41421L1.70711 13.7071C1.31658 14.0976 0.683418 14.0976 0.292893 13.7071C-0.0976309 13.3166 -0.0976309 12.6834 0.292893 12.2929L5.58579 7L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683418 0.292893 0.292893Z" fill="#777E91" />
+                            </svg>
+                        </button>
                     </div>
                 </nav>
                 <div className={`${styles.overlay} ${showNavbar ? styles.show : ''}`} onClick={handleShowNavbar} />
